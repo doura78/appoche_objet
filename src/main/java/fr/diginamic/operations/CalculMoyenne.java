45822f2aefe68479f0f5b596f7bfe2e0c2c26492
Abdouramane SIDIBE <sidibeabdouramane@hotmail.fr>
2026-06-26 14:05:37 +0200

    tp9 calculeMoyenne

--- src/main/java/fr/diginamic/operations/CalculMoyenne.java
+++ src/main/java/fr/diginamic/operations/CalculMoyenne.java
@@ -4,23 +4,32 @@ import java.util.Arrays;
 
 public class CalculMoyenne {
 
+    // Tableau de nombres décimaux stockant les valeurs ajoutées
     private double[] array = new double[0];
 
+    // Ajoute un nouvel élément au tableau en l'agrandissant d'une case
     public void ajout(double element) {
         array = Arrays.copyOf(array, array.length + 1);
         array[array.length - 1] = element;
     }
 
-    public  double calcul() {
+    // Calcule et retourne la moyenne des valeurs du tableau
+    public double calcul() {
+        // Si le tableau est vide, on retourne 0.0 pour éviter une division par zéro
         if (array.length == 0) {
             return 0.0;
         }
+
+        // Variable qui servira à additionner tous les éléments du tableau
         double somme = 0;
 
+        // Parcours du tableau pour calculer la somme totale
         int i;
         for (i = 0; i < array.length; i++) {
             somme = somme + array[i];
         }
+
+        // Calcul de la moyenne : somme divisée par le nombre d'éléments
         return somme / array.length;
     }
 
--- src/test/java/TestMoyenne.java
+++ src/test/java/TestMoyenne.java
@@ -2,17 +2,26 @@ import fr.diginamic.operations.CalculMoyenne;
 
 public class TestMoyenne {
     public static void main(String[] args) {
+        // Création d'une première instance de CalculMoyenne
         CalculMoyenne moyenne1 = new CalculMoyenne();
+
+        // Ajout de plusieurs valeurs dans le premier objet
         moyenne1.ajout(10);
         moyenne1.ajout(24);
         moyenne1.ajout(30.35);
 
+        // Affichage de la moyenne calculée pour le premier test
         System.out.println(moyenne1.calcul());
 
+        // Création d'une deuxième instance de CalculMoyenne
         CalculMoyenne moyenne2 = new CalculMoyenne();
+
+        // Ajout d'autres valeurs dans le deuxième objet
         moyenne2.ajout(59);
         moyenne2.ajout(125.36);
         moyenne2.ajout(256.74);
+
+        // Affichage de la moyenne calculée pour le deuxième test
         System.out.println(moyenne2.calcul());
     }
 }
